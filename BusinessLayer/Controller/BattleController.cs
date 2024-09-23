b3099471a031a1b21db350afd33f0939e061f22a
Florian Mimmler <if23b241@technikum-wien.at>
2024-09-23 10:42:56 +0200

    layerd structure and singleton pattern implemented

--- BusinessLayer/Controller/BattleController.cs
+++ BusinessLayer/Controller/BattleController.cs
@@ -6,8 +6,14 @@ using System.Threading.Tasks;
 
 namespace MTCG.Battle
 {
-    internal class Battle
+    internal class BattleController
     {
+        private static BattleController _instance;
+
+        public static BattleController Instance => _instance ?? (_instance = new BattleController());
+
+        private BattleController() {}
+
         public User Player1 { get; set; }
         public User Player2 { get; set; }
     }
